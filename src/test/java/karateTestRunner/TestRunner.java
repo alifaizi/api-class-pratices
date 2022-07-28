4ea594fc0bb90d9ba5fca580a885c7ab64f14f67
Ali Faizi <janalifaizi@gmail.com>
2022-07-27 21:48:16 -0400

    Updated the my project files

--- src/test/java/karateTestRunner/TestRunner.java
+++ src/test/java/karateTestRunner/TestRunner.java
@@ -3,7 +3,7 @@ package karateTestRunner;
 import com.intuit.karate.junit5.Karate;
 
 public class TestRunner {
-
+// This test runner is for tiger-api-test project
 	@Karate.Test
 	Karate runMyTest() {
 		return Karate
